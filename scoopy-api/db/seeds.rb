7b8c0dd7d7c86683db0e22e14331692e9a756506
Enrique Caño Vaquero <Enriquecano@Enrique-Cano.local>
2026-07-28 08:58:07 +0200

    [#12] Feature: Added configuration for development database. - Added seed for development database.

--- scoopy-api/db/seeds.rb
+++ scoopy-api/db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+return unless Rails.env.development?
+
+product = Product.create(name:'test')
+provider = Provider.create(name:'Amazon', url:'https://www.amazon.es')
+ProvidersProduct.create(product_id:product.id, provider_id:provider.id, ssn:'test123')
+PriceHistory.create(product_id:product.id, providers_id:provider.id, price: 10.0, currency: 'EUR')
+User.create!(email:'test@example.com',password:'123456')
