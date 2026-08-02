9b11e2d1447878e448753370e94550e0201219af
Enrique Caño <74306002+Enriquecava@users.noreply.github.com>
2026-08-02 16:05:43 +0200

    [#17] Feature: Added translations for en-es.

--- scoopy-api/config/initializers/cors.rb
+++ scoopy-api/config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://127.0.0.1:5173"
+    origins "http://localhost:5137", "http://127.0.0.1:5137", "http://localhost:5173", "http://127.0.0.1:5173"
 
     resource "*",
       headers: :any,
       methods: %i[get post put patch delete options head],
-      expose: %w[Authorization]
+      expose: %w[Authorization],
+      max_age: 600
   end
 end
